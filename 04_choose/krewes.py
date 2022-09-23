b12fdd91d1dc7f859cb2348cba0870ff126d6c45
Yuki Feng <yuki.2102005@gmail.com>
2022-09-22 22:36:39 -0400

    04 hw

---
+++ 04_choose/krewes.py
@@ -0,0 +1,25 @@
+import random
+
+"""
+TNPG: sdy (Sebastian, David, Yuki)
+K0?
+2022-09-22
+time spent:
+
+DISCO:
+- Keys of a dictionary can be accessed just like an array
+- Libraries are known as modules
+
+QCC:
+- Where are we getting our devos?
+
+OPS SUMMARY:
+- Attempt to write code
+- Consult QAF
+- Write correct code
+"""
+
+krewes = {2: ['Sebastian', 'David', 'Yuki'],
+          7: ['David', 'Yuki', 'Sebastian'],
+          8: ['Yuki', 'Sebastian', 'David']}
+print(krewes[2][random.randint(0, 2)])
