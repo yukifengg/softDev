a694ec74c2343076aa0e00a7f690d91334b2376e
yukifengg <yuki.2102005@gmail.com>
2022-11-07 13:40:52 -0500

    hk

--- 19_session/app.py
+++ 19_session/app.py
@@ -1,42 +1,61 @@
 '''
 Metamorphosis: Eric Sohel, Yuki Feng, Maya Mori, Aleksandra Shifrina
 Softdev
-k19: blah blah
+k19 -- sessions in Flask
 2022-11-04
-time spent: 0.8
+time spent: 2.0
 '''
 
 from flask import Flask             #facilitate flask webserving
-from flask import request, redirect
+from flask import render_template 
+from flask import request
 from flask import session
 
+import os
+
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route("/")
+app.secret_key = os.urandom(32)
+username = 'bob'
+password = 'mob123'
+
+# displaying the homepage
+@app.route('/', methods=['GET'])
 def index():
+    print(session)
+    # print(request.cookies)
+    print(request.cookies.get('username'))
     if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
+        return render_template('response.html',username=session["username"])
+    return render_template('login.html',has_error=False)
 
-@app.route('/login', methods=['GET', 'POST'])
+# if user tried to login in
+@app.route('/', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-        # return render_template("login.html")
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
+    print('login function')
 
-@app.route('/logout')
+    # if everything matches
+    if request.form['username'] == username and request.form['password'] == password:
+        # add session
+        session['username'] = request.form['username']
+        return render_template('response.html',username=session["username"])
+
+    # handle cases when username/password in wrong
+    error_type = ''     # error message
+    if request.form['username'] != username:
+        error_type='Username not found'
+    elif request.form['password'] != password:
+        error_type = 'Wrong password'
+    return render_template('login.html',has_error=True,error=error_type)
+    # session['username'] = request.form['username']
+
+@app.route('/logout',methods=['POST'])
 def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
-    return redirect(url_for('index'))
+    return render_template('login.html')
 
-def disp_loginpage():
-    request.cookies.get()
+
+if __name__ == "__main__": 
+    app.debug = True
+    app.run()
---
+++ 19_session/readme.md
@@ -0,0 +1,8 @@
+DISCO:
+- sessions works like a dictionary
+- Can have separate functions with same route
+- `b` before a string means it is a byte literal
+- `KeyError` isn't raised if you put `None` as a 2nd argument in `session.pop()`
+
+QCC:
+- Is there a better way to create a logout button other than making an HTML form and using the submit button?
