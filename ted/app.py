93ce73f1976532e6ac57f99cb45de0ae1af145b8
Yuki Feng <yuki.2102005@gmail.com>
2023-03-16 01:51:19 -0400

    poop

--- ted/app.py
+++ ted/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")       
 def hello_world():
-    return render_template('index.html')
+    return render_template('sassy_lassie.html')
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
