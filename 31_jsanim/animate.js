9e8f553032ce1c414bac0717a03b2f3e6858547b
Yuki Feng <yuki.2102005@gmail.com>
2023-04-25 16:34:00 -0400

    hooray

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -17,30 +17,47 @@ var requestID; //init global var for use with animation frames
 
 var clear = (e) => {
     ctx.clearRect(0,0,500,500);
-}
+};
 
 var radius = 0;
 var growing = "true";
 
 var drawDot = () => {
-    clear();
-    ctx.beginPath();
-    if (radius <= 250) {
-        radius += 1
-        ctx.arc(250, 250, radius, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.stroke();
-        requestAnimationFrame(drawDot)
+    clear()
+    ctx.beginPath()
+    ctx.arc(250, 250, radius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    if (growing === "true") {
+        if (radius < 250) {
+            radius += 1
+            requestID = window.requestAnimationFrame(drawDot)
+        }
+        else if (radius == 250) {
+            growing = "false"
+            requestID = window.requestAnimationFrame(drawDot)
+        }
     }
-    else {
-        growing = "false";
-        cancelAnimationFrame(drawDot)
+    else if (growing === "false") {
+        if (radius > 0) {
+            radius -= 1;
+            // ctx.arc(250, 250, radius, 0, 2 * Math.PI);
+            // ctx.fill();
+            // ctx.stroke();
+            requestID = window.requestAnimationFrame(drawDot)
+        }
+        else if (radius == 0) {
+            growing = "true"
+            requestID = window.requestAnimationFrame(drawDot)
+        }
     }
+    
 }
 
 var stopIt = () => {
     console.log("stopIt invoked...")
     console.log(requestID);
+    window.cancelAnimationFrame(requestID)
 }
 
 dotButton.addEventListener("click", drawDot);
